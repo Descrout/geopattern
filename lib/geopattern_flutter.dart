3b27aeb834d8253e5a38b8634ef6b23cfb68a4a5
Shellix Dev <shellixdev@192.168.1.101>
2023-04-26 16:31:07 +0300

    migrated to null stafety

--- lib/geopattern_flutter.dart
+++ lib/geopattern_flutter.dart
@@ -4,13 +4,11 @@ import 'package:flutter/widgets.dart';
 
 import 'patterns/pattern.dart';
 
-/// A CustomPainter that takes a single pattern and draws it across the entire canvas.
 class FullPainter extends CustomPainter {
   Color background;
   Pattern pattern;
 
-  FullPainter({@required this.pattern, @required this.background})
-      : assert(pattern != null && background != null);
+  FullPainter({required this.pattern, required this.background});
 
   @override
   void paint(Canvas canvas, Size size) {
